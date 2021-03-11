5ff30e57f821ee9cfd4e68714b2ca8fa7b55da9d
maheshpsgithub@gmail.com <maheshpsgithub@gmail.com>
2021-03-11 13:09:20 +0530

    first commit

---
+++ RedisBasicClient1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using StackExchange.Redis;
+
+
+namespace RedisBasicClient1
+{
+    class Program
+    {
+        static readonly ConnectionMultiplexer redis = ConnectionMultiplexer.Connect("localhost:6379");  //ConnectionMultiplexer.Connect("hostname:port,password=password");
+        static void Main(string[] args)
+        {
+
+            IDatabase conn = redis.GetDatabase();
+
+            //SET AND GET STRING VALUE
+            conn.StringSet("foo", "bar");
+            var value = conn.StringGet("foo");
+            Console.WriteLine("values of foo:" + value);
+
+            ISubscriber sub = redis.GetSubscriber();
+
+            sub.Publish("messages", "hello");
+
+
+            Console.WriteLine("Preass any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ RedisBasicClient2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using StackExchange.Redis;
+
+
+namespace RedisBasicClient2
+{
+    class Program
+    {
+        static readonly ConnectionMultiplexer redis = ConnectionMultiplexer.Connect("localhost:6379");  //ConnectionMultiplexer.Connect("hostname:port,password=password");
+        static void Main(string[] args)
+        {
+
+            ISubscriber sub = redis.GetSubscriber();
+
+            sub.Subscribe("messages", (channel, message) => {
+                Console.WriteLine((string)message);
+            });
+
+            Console.WriteLine("Preass any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
